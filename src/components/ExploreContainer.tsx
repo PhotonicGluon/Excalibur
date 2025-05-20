6f9bee60ef5b5e60af70be417dc277daa449a018
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2025-05-20 09:50:20 +0800

    Add TailwindCSS support

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,24 @@
-import './ExploreContainer.css';
+import "./ExploreContainer.css";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
-  return (
-    <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
+    return (
+        <div className="absolute top-1/2 right-0 left-0 -translate-y-1/2 text-center">
+            <strong className="text-2xl font-bold">Ready to create an app?</strong>
+            <p className="m-0 text-base text-gray-400">
+                Start with Ionic{" "}
+                <a
+                    className="no-underline"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://ionicframework.com/docs/components"
+                >
+                    UI Components
+                </a>
+            </p>
+        </div>
+    );
 };
 
 export default ExploreContainer;
