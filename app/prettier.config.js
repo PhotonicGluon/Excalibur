548e38e9c3f747ae5e8358c53848d3410049fa34
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2025-05-25 17:33:41 +0800

    Add prettier plugin to sort JS/TS imports

---
+++ app/prettier.config.js
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    plugins: ["prettier-plugin-tailwindcss", "@trivago/prettier-plugin-sort-imports"],
+    tabWidth: 4,
+    printWidth: 120,
+    // Import ordering
+    importOrder: ["^@lib/(.*)$", "^@components/(.*)$", "^@pages/(.*)$", "^[./]"],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+};
+
+export default config;
