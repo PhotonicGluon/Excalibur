22e7f8dd4fa470e375ffde6b690a6b3131403646
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2025-05-30 15:02:10 +0800

    Add a timeout for logging in to server

--- app/src/lib/network.ts
+++ app/src/lib/network.ts
@@ -2,12 +2,13 @@
  * Checks if the given URL is reachable.
  *
  * @param url The URL to check.
+ * @param timeout The timeout in seconds.
  * @returns `true` if the URL is reachable, `false` otherwise.
  */
-export async function checkConnection(url: string): Promise<boolean> {
+export async function checkConnection(url: string, timeout: number = 5): Promise<boolean> {
     // Try to connect
     try {
-        await fetch(url);
+        await fetch(url, { signal: AbortSignal.timeout(timeout * 1000) });
         return true;
     } catch (e) {
         console.error(e);
