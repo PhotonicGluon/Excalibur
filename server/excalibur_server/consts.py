069cf2fd3f1c4d5907dfa4be91725ac39bd48f97
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2025-08-03 11:51:21 +0800

    Allow root folder to be specified using an environment variable

--- server/excalibur_server/consts.py
+++ server/excalibur_server/consts.py
@@ -1,6 +1,7 @@
+import os
 from pathlib import Path
 
-ROOT_FOLDER = Path("excalibur-files").resolve()
+ROOT_FOLDER = Path(os.getenv("EXCALIBUR_ROOT_FOLDER", "excalibur-files")).resolve()
 FILES_FOLDER = ROOT_FOLDER / "vault"
 
 MAX_FILE_SIZE = 50_000 * 1024  # 50 MiB
