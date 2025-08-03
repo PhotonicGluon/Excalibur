0ebb607cbcbd037b2a610b9348f9f1bc13dd725b
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2025-08-03 11:31:36 +0800

    Fix issue with git repository commit retrieval

--- server/excalibur_server/meta.py
+++ server/excalibur_server/meta.py
@@ -2,7 +2,6 @@ from importlib.metadata import metadata
 
 import git
 
-repo = git.Repo(search_parent_directories=True)
 pkg_metadata = metadata("excalibur-server")
 
 TITLE = "Excalibur Server"
@@ -10,6 +9,10 @@ SUMMARY = pkg_metadata["Summary"]
 VERSION = pkg_metadata["Version"]
 
 try:
+    repo = git.Repo(search_parent_directories=True)
     COMMIT = repo.head.object.hexsha  # 40-byte commit hash
-except ValueError:  # Occurs if the repository is not a git repository (e.g., bundled as a wheel)
+except (
+    ValueError,
+    git.InvalidGitRepositoryError,
+):  # Occurs if the repository is not a git repository (e.g., bundled as a wheel)
     COMMIT = None
