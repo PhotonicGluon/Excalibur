907ab9ce6d2b9a9ae4fb3cfb09dc1280831d580c
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2025-08-13 17:26:33 +0800

    Exclude test files in distributable

--- server/excalibur_server/cli/__init__.py
+++ server/excalibur_server/cli/__init__.py
@@ -22,7 +22,12 @@ from .init_server import init_server as init_server
 from .setup_server import setup_server as setup_server
 from .start_server import start_server as start_server
 from .reset_server import reset_server as reset_server
-from .run_tests import run_tests as run_tests
+
+# Since tests may be excluded from the build, we need to handle the import like this
+try:
+    from .run_tests import run_tests as run_tests
+except ImportError:
+    pass
 
 
 __all__ = ["app"]
