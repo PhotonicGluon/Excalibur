82f19ec78ae0bd55c28e25ba6f487549b368fc84
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2025-08-17 19:33:00 +0800

    Add better detection of missing `config.toml` file

--- server/excalibur_server/src/config/__init__.py
+++ server/excalibur_server/src/config/__init__.py
@@ -1,5 +1,15 @@
-from .config import Config
+from pydantic import ValidationError
 
-CONFIG = Config()
+from .config import Config, CONFIG_FILE
+
+try:
+    CONFIG = Config()
+except ValidationError as e:
+    # Check if config file exists
+    if not CONFIG_FILE.exists():
+        raise FileNotFoundError(f"Config file not found at '{CONFIG_FILE}'. Did you initialize the server?")
+    
+    # Seems like the config file is missing fields
+    raise e
 
 __all__ = ["CONFIG"]
