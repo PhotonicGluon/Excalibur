b349aa2e6f0d24953ff7d25d451c01d38a72c983
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2025-08-30 17:02:26 +0800

    Create `FSNameable` table

--- server/excalibur_server/src/db/tables.py
+++ server/excalibur_server/src/db/tables.py
@@ -17,8 +17,26 @@ class User(SQLModel, table=True):
     "Salt for the Secure Remote Password (SRP) protocol key"
     srp_verifier: bytes = Field(nullable=False)
     "Verifier to prove server's identity in SRP"
-    key_enc: bytes = Field(nullable=False)  # TODO: Set maximum length
+    key_enc: bytes = Field(nullable=False)
     """
     Encrypted vault key as an ExEF stream.
     The vault key should have been encrypted using the Account Unlock Key (AUK).
     """
+
+
+class FSNameable(SQLModel, table=True):
+    """
+    A filesystem nameable (e.g., a file or folder).
+    """
+
+    uuid: str = Field(primary_key=True)
+    "UUID of the nameable"
+    name: str = Field(nullable=False)
+    "Real name of the nameable"
+
+    @property
+    def fs_name(self) -> str:
+        """
+        Returns the name of the nameable in the file system.
+        """
+        return self.uuid
