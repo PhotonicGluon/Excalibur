eafcc0483db72f478d8f9851aa71a0c5909ecc95
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2025-09-10 13:25:43 +0800

    Update vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -38,8 +38,6 @@ export default defineConfig({
     },
     test: {
         globals: true,
-        environment: "jsdom",
-        setupFiles: ["./src/vitest-setup.ts"],
     },
     server: { watch: { ignored: ["**/android"] } },
     build: {
