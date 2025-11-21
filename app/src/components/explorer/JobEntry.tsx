0f08a213bd6df6a90ce11b4ad1ce6a686bd3c30f
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2025-11-21 10:56:46 +0800

    Add button to cancel jobs

--- app/src/components/explorer/JobEntry.tsx
+++ app/src/components/explorer/JobEntry.tsx
@@ -1,5 +1,7 @@
+import { Capacitor } from "@capacitor/core";
+
 import { IonIcon, IonLabel } from "@ionic/react";
-import { arrowDown, arrowUp } from "ionicons/icons";
+import { arrowDown, arrowUp, closeCircleOutline } from "ionicons/icons";
 
 import CircularProgressBar from "@components/CircularProgressBar";
 
@@ -19,16 +21,35 @@ export interface Job {
     progress: number | null;
 }
 
-const JobEntry: React.FC<Job> = (job) => {
+interface ContainerProps extends Job {
+    /** Function to call when the job is cancelled */
+    onCancel: () => void;
+}
+
+const JobEntry: React.FC<ContainerProps> = (props) => {
     return (
         <div className="grid h-6 grid-cols-2">
-            <div className="flex max-w-40 items-center gap-1">
-                <IonIcon icon={job.direction === "upload" ? arrowUp : arrowDown} className="size-4"></IonIcon>
-                <IonLabel className="truncate font-mono font-bold">{job.filename}</IonLabel>
+            <div className="flex items-center gap-1">
+                <IonIcon icon={props.direction === "upload" ? arrowUp : arrowDown} className="size-4"></IonIcon>
+                <IonLabel className="max-w-36 truncate font-mono font-bold" title={props.filename}>
+                    {props.filename}
+                </IonLabel>
             </div>
             <div className="flex items-center">
-                <IonLabel className="grow">{job.description}</IonLabel>
-                <CircularProgressBar className="size-6" value={job.progress} />
+                <IonLabel className="grow">{props.description}</IonLabel>
+                <div
+                    className="group relative size-6 *:absolute *:top-0 *:left-0 *:size-full hover:cursor-pointer"
+                    onClick={() => props.onCancel()}
+                >
+                    <CircularProgressBar value={props.progress} />
+                    <IonIcon
+                        icon={closeCircleOutline}
+                        className={
+                            "-z-10 transition-opacity duration-100 group-hover:opacity-100 " +
+                            (Capacitor.isNativePlatform() ? "opacity-100" : "opacity-0")
+                        }
+                    ></IonIcon>
+                </div>
             </div>
         </div>
     );
