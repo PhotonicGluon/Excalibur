5d542c62877fdbf4a57b25d2f2395fefb5931621
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2026-04-08 18:40:26 +0800

    Create substitution cipher implementation for name obfuscation

---
+++ app/packages/main/src/lib/files/obfuscation.test.ts
@@ -0,0 +1,22 @@
+import { expect } from "vitest";
+
+import { SubstitutionCipher } from "./obfuscation";
+
+describe("SubstitutionCipher", () => {
+    it("should encrypt and decrypt", () => {
+        const cipher = new SubstitutionCipher(Buffer.from("test"));
+        const pt = Buffer.from("hello");
+        const ct = cipher.encipher(pt);
+        const pt2 = cipher.decipher(ct);
+        expect(pt2).toEqual(pt);
+    });
+
+    it("should produce same ciphertext given same seed", () => {
+        const cipher1 = new SubstitutionCipher(Buffer.from("test"));
+        const cipher2 = new SubstitutionCipher(Buffer.from("test"));
+        const pt = Buffer.from("hello");
+        const ct1 = cipher1.encipher(pt);
+        const ct2 = cipher2.encipher(pt);
+        expect(ct1).toEqual(ct2);
+    });
+});
---
+++ app/packages/main/src/lib/files/obfuscation.ts
@@ -0,0 +1,62 @@
+import seedrandom from "seedrandom";
+
+/**
+ * Substitution cipher for obfuscation.
+ */
+export class SubstitutionCipher {
+    private _prng: seedrandom.PRNG;
+    private _forwardCipher: number[];
+    private _backwardCipher: number[];
+
+    /**
+     * Creates a new substitution cipher with the given seed.
+     *
+     * @param seed seed to use for the cipher
+     */
+    constructor(seed: Buffer) {
+        this._prng = seedrandom(seed.toString());
+
+        // Create the substitution maps
+        this._forwardCipher = Array.from({ length: 256 }, (_, i) => i);
+        this._backwardCipher = Array.from({ length: 256 });
+
+        this._shuffle(this._forwardCipher);
+        for (let i = 0; i < 256; i++) {
+            this._backwardCipher[this._forwardCipher[i]] = i;
+        }
+    }
+
+    // Helper methods
+    /**
+     * Randomly shuffles an array in place using the Fisher–Yates shuffle algorithm.
+     *
+     * @param array array to shuffle
+     */
+    private _shuffle(array: number[]) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(this._prng() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    // Main methods
+    /**
+     * Enciphers a buffer using substitution cipher.
+     *
+     * @param pt plaintext to encipher
+     * @returns ciphertext
+     */
+    encipher(pt: Buffer): Buffer {
+        return Buffer.from(pt.map((b) => this._forwardCipher[b]));
+    }
+
+    /**
+     * Deciphers a buffer using substitution cipher.
+     *
+     * @param ct ciphertext to decipher
+     * @returns plaintext
+     */
+    decipher(ct: Buffer): Buffer {
+        return Buffer.from(ct.map((b) => this._backwardCipher[b]));
+    }
+}
