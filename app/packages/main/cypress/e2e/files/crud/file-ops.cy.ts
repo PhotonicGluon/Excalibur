c6722a75637957ff3e30cfb46eeb80d98d129f5e
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2026-05-01 20:59:39 +0800

    Add more Cypress E2E tests

--- app/packages/main/cypress/e2e/files/crud/file-ops.cy.ts
+++ app/packages/main/cypress/e2e/files/crud/file-ops.cy.ts
@@ -1,4 +1,4 @@
-import { createFile } from "../helpers";
+import { createFile, createFolder } from "../helpers";
 import { LARGE_SIZE, SMALL_SIZE } from "./constants";
 
 describe("Upload and Download Operations", () => {
@@ -34,5 +34,108 @@ describe("Upload and Download Operations", () => {
         it("should handle large files", () => {
             createFile(Array.from({ length: 3 }, () => LARGE_SIZE));
         });
+    });
+});
+
+describe("Rename Operations", () => {
+    beforeEach(() => {
+        cy.login("http://127.0.0.1:8989", "test-user", "Password");
+        cy.visit("/files/");
+        cy.url().should("include", "/files");
+    });
+
+    afterEach(function () {
+        // Stop other tests if any test fails
+        if (this.currentTest.state === "failed") {
+            Cypress.stop();
+            return;
+        }
+    });
+
+    it("should rename item", () => {
+        // Create test item
+        const folderName = createFolder();
+        cy.get(`div[data-name='${folderName}']`).should("exist");
+
+        // Click on rename button
+        cy.get(`div[data-name='${folderName}']`).scrollIntoView(); // Make sure its visible
+        cy.wait(100); // Make sure scroll completes
+        cy.get(`div[data-name='${folderName}']`).rightclick();
+        cy.get(".item").contains("Rename").click();
+
+        cy.get(".alert-head").should("have.text", "Enter New Name");
+
+        // Enter test folder name
+        const newName = `New Name ${Date.now()}`;
+        cy.get(".alert-input-wrapper").click().wait(100); // For the focus to appear
+        cy.get(".alert-input-wrapper").type("{selectAll}" + newName);
+        cy.get(".alert-button-group").contains("Rename").click();
+        cy.get(".alert-head").should("not.exist");
+
+        cy.get(`div[data-name='${folderName}']`).should("not.exist");
+        cy.get(`div[data-name='${newName}']`).should("exist");
+    });
+});
+
+describe("Deletion Operations", () => {
+    beforeEach(() => {
+        cy.login("http://127.0.0.1:8989", "test-user", "Password");
+        cy.visit("/files/");
+        cy.url().should("include", "/files");
+    });
+
+    afterEach(function () {
+        // Stop other tests if any test fails
+        if (this.currentTest.state === "failed") {
+            Cypress.stop();
+            return;
+        }
+    });
+
+    it("should delete file", () => {
+        // Create file
+        const [fileName] = createFile(SMALL_SIZE);
+        cy.get(`div[data-name='${fileName}']`).should("exist");
+
+        // Click on delete button
+        cy.get(`div[data-name='${fileName}']`).scrollIntoView(); // Make sure its visible
+        cy.wait(100); // Make sure scroll completes
+        cy.get(`div[data-name='${fileName}']`).rightclick();
+        cy.get(".item").contains("Delete").click();
+
+        cy.get(`div[data-name='${fileName}']`).should("not.exist");
+    });
+
+    it("should delete empty folder", () => {
+        // Create folder
+        const folderName = createFolder();
+        cy.get(`div[data-name='${folderName}']`).should("exist");
+
+        // Click on delete button
+        cy.get(`div[data-name='${folderName}']`).scrollIntoView(); // Make sure its visible
+        cy.wait(100); // Make sure scroll completes
+        cy.get(`div[data-name='${folderName}']`).rightclick();
+        cy.get(".item").contains("Delete").click();
+
+        cy.get(`div[data-name='${folderName}']`).should("not.exist");
+    });
+
+    it("should delete non-empty folder", () => {
+        // Create folder
+        const folderName = createFolder();
+        cy.get(`div[data-name='${folderName}']`).click();
+        createFolder(); // Creates a folder within the original folder
+        cy.get("#files-area").contains("(Go Back)").click();
+
+        // Click on delete button
+        cy.get(`div[data-name='${folderName}']`).scrollIntoView(); // Make sure its visible
+        cy.wait(100); // Make sure scroll completes
+        cy.get(`div[data-name='${folderName}']`).rightclick();
+        cy.get(".item").contains("Delete").click();
+
+        // Confirm the deletion
+        cy.get(".alert-wrapper").should("exist");
+        cy.get(".alert-wrapper").contains("Delete").click();
+        cy.get(`div[data-name='${folderName}']`).should("not.exist");
     });
 });
