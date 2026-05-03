75c145cf3291feae507491e3645be7a1cf7883a6
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2026-05-03 15:05:58 +0800

    Create skeleton of new sorting popup

--- app/packages/main/src/components/explorer/DirectoryList.tsx
+++ app/packages/main/src/components/explorer/DirectoryList.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-import { IonCol, IonGrid, IonIcon, IonLabel, IonRow } from "@ionic/react";
-import { alertCircleOutline, arrowDown, arrowUp, checkmarkCircleOutline } from "ionicons/icons";
+import { IonCol, IonContent, IonGrid, IonIcon, IonItem, IonLabel, IonList, IonRow, useIonPopover } from "@ionic/react";
+import { alertCircleOutline, arrowDown, arrowUp, checkmark, checkmarkCircleOutline } from "ionicons/icons";
 
 import { Directory } from "@lib/files/structures";
 
@@ -21,6 +21,56 @@ interface ContainerProps {
     listenerConnected: boolean;
 }
 
+const SortOptionsPopover: React.FC<{
+    sortAsc: boolean;
+    setSortAsc: (sortAsc: boolean) => void;
+    onDismissPopover: () => void;
+}> = ({ sortAsc, setSortAsc, onDismissPopover }) => (
+    <IonContent>
+        <IonList lines="none" className="h-full">
+            <IonItem className="-mb-2">
+                <IonLabel className="font-bold">Sort by</IonLabel>
+            </IonItem>
+            <IonList lines="none">
+                {/* TODO: Add other sort options */}
+                <IonItem
+                    button={true}
+                    onClick={() => {
+                        console.log("Sorting by name");
+                        onDismissPopover();
+                    }}
+                >
+                    <IonIcon className="size-6 pr-2" icon={checkmark}></IonIcon>
+                    <IonLabel>Name</IonLabel>
+                </IonItem>
+            </IonList>
+            <hr className="my-0 bg-neutral-400 dark:bg-neutral-500" />
+            <IonList lines="none">
+                <IonItem
+                    button={true}
+                    onClick={() => {
+                        setSortAsc(true);
+                        onDismissPopover();
+                    }}
+                >
+                    <IonIcon className="size-6 pr-2" icon={sortAsc ? checkmark : undefined}></IonIcon>
+                    <IonLabel>A to Z {/* TODO: Customize label */}</IonLabel>
+                </IonItem>
+                <IonItem
+                    button={true}
+                    onClick={() => {
+                        setSortAsc(false);
+                        onDismissPopover();
+                    }}
+                >
+                    <IonIcon className="size-6 pr-2" icon={sortAsc ? undefined : checkmark}></IonIcon>
+                    <IonLabel>Z to A {/* TODO: Customize label */}</IonLabel>
+                </IonItem>
+            </IonList>
+        </IonList>
+    </IonContent>
+);
+
 const DirectoryList: React.FC<ContainerProps> = (props: ContainerProps) => {
     // States
     const [sortAsc, setSortAsc] = useState(true);
@@ -29,15 +79,26 @@ const DirectoryList: React.FC<ContainerProps> = (props: ContainerProps) => {
     const explorerContext = useExplorerContext();
 
     // Render
+    const [showPopover, dismissPopover] = useIonPopover(SortOptionsPopover, {
+        sortAsc: sortAsc,
+        setSortAsc: setSortAsc,
+        onDismissPopover: () => dismissPopover(),
+    });
     return (
         <div id={props.id}>
             {/* Sorting Buttons */}
             <IonGrid className="pb-1">
                 <IonRow className="ion-align-items-center">
                     <IonCol className="ml-2 flex items-center font-bold">
-                        <div className="flex items-center hover:cursor-pointer" onClick={() => setSortAsc(!sortAsc)}>
+                        <div
+                            className="flex items-center hover:cursor-pointer"
+                            onClick={(e) => showPopover({ event: e.nativeEvent, reference: "event", side: "bottom" })}
+                        >
                             <IonLabel>Name</IonLabel>
-                            <IonIcon className="ml-1 size-6" icon={sortAsc ? arrowUp : arrowDown} />
+                            <IonIcon
+                                className="ml-1 size-4 rounded-full bg-blue-500/50 p-1"
+                                icon={sortAsc ? arrowUp : arrowDown}
+                            />
                         </div>
                     </IonCol>
                     {props.directory && props.directory.items && (
