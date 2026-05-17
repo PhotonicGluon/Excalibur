5433daa11b47a96c5659ec88caa752eb9f0b74ca
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2026-05-17 13:12:37 +0800

    Add new item count endpoint

---
+++ server/changelog.d/+item_count_endpoint.added.md
@@ -0,0 +1,1 @@
+✨ Added a new endpoint `/api/files/count` to count the number of items in the user's file system.
---
+++ server/excalibur_server/api/routes/files/count.py
@@ -0,0 +1,30 @@
+from typing import Annotated
+
+from fastapi import Depends, status
+
+from excalibur_server.api.routes.files import encrypted_router
+from excalibur_server.src.auth.credentials import Credentials, get_credentials
+from excalibur_server.src.db.operations import get_items_in_root
+from excalibur_server.src.users import get_user
+
+
+@encrypted_router.get(
+    "/count",
+    name="Count All Items",
+    responses={
+        status.HTTP_200_OK: {
+            "content": {"application/json": {"example": 123}},
+        },
+    },
+    response_model=int,
+)
+def count_all_items_endpoint(credentials: Annotated[Credentials, Depends(get_credentials)]) -> int:
+    """
+    Gets the number of items owned by the authenticated user.
+    """
+
+    username = credentials.username
+
+    root_id = get_user(username).fsitem_id
+    fsitems = get_items_in_root(root_id)
+    return len(fsitems)
