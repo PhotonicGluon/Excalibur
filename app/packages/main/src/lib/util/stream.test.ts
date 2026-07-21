eccc7972f5dc24db3c32755a236941cdba3b83eb
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2026-07-21 20:30:54 +0800

    Update `buffer.ts` and `stream.ts`

--- app/packages/main/src/lib/util/stream.test.ts
+++ app/packages/main/src/lib/util/stream.test.ts
@@ -1,27 +1,44 @@
 import { expect } from "vitest";
 
-import { chunkStream } from "./stream";
+import { chunkData, chunkStream, collectStream } from "./stream";
+
+const TEST_DATA = Buffer.from(
+    "All human beings are born free and equal in dignity and rights. They are endowed with reason and conscience and should act towards one another in a spirit of brotherhood.",
+    "utf-8",
+);
+
+describe("chunkData", () => {
+    const streamChunkSizes = [1, 4, 16, 64];
+
+    for (const streamChunkSize of streamChunkSizes) {
+        it(`should work for a stream chunk of ${streamChunkSize}`, async () => {
+            const iterable = chunkData(TEST_DATA, streamChunkSize);
+            const stream = iterable.getReader();
+
+            let i = 0;
+            while (true) {
+                const { done, value } = await stream.read();
+                if (done) {
+                    break;
+                }
+
+                const streamOut = Buffer.from(value).toString("utf-8");
+                const expectOut = Buffer.from(TEST_DATA.subarray(i, i + streamChunkSize)).toString("utf-8");
+                expect(streamOut).toEqual(expectOut);
+                i += streamChunkSize;
+            }
+        });
+    }
+});
 
 describe("chunkStream", () => {
-    const data = Buffer.from(
-        "All human beings are born free and equal in dignity and rights. They are endowed with reason and conscience and should act towards one another in a spirit of brotherhood.",
-        "utf-8",
-    );
     const streamChunkSizes = [1, 4, 16, 64];
     const outputChunkSizes = [1, 4, 16, 64];
 
     for (const streamChunkSize of streamChunkSizes) {
         for (const outputChunkSize of outputChunkSizes) {
-            test(`stream chunk ${streamChunkSize}, output chunk ${outputChunkSize}`, async () => {
-                const iterable = new ReadableStream<Uint8Array>({
-                    start(controller) {
-                        for (let i = 0; i < data.length; i += streamChunkSize) {
-                            controller.enqueue(data.subarray(i, i + streamChunkSize));
-                        }
-                        controller.close();
-                    },
-                });
-
+            it(`should work for a stream chunk of ${streamChunkSize} and output chunk of ${outputChunkSize}`, async () => {
+                const iterable = chunkData(TEST_DATA, streamChunkSize);
                 const stream = chunkStream(iterable, outputChunkSize);
                 const reader = stream.getReader();
 
@@ -33,11 +50,23 @@ describe("chunkStream", () => {
                     }
 
                     const streamOut = Buffer.from(value).toString("utf-8");
-                    const expectOut = Buffer.from(data.subarray(i, i + outputChunkSize)).toString("utf-8");
+                    const expectOut = Buffer.from(TEST_DATA.subarray(i, i + outputChunkSize)).toString("utf-8");
                     expect(streamOut).toEqual(expectOut);
                     i += outputChunkSize;
                 }
             });
         }
+    }
+});
+
+describe("collectStream", () => {
+    const streamChunkSizes = [1, 4, 16, 64];
+
+    for (const streamChunkSize of streamChunkSizes) {
+        it(`should collects all data for a stream chunk of ${streamChunkSize}`, async () => {
+            const iterable = chunkData(TEST_DATA, streamChunkSize);
+            const collected = await collectStream(iterable);
+            expect(collected).toEqual(TEST_DATA);
+        });
     }
 });
